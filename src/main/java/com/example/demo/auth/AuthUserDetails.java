e335c9d83401c1762998cfb1a87af917e7f5b9c0
katafuchix <katafuchix@gmail.com>
2023-08-04 12:11:52 +0900

    認証系処理の整理、テンプレート整理

---
+++ src/main/java/com/example/demo/auth/AuthUserDetails.java
@@ -0,0 +1,88 @@
+package com.example.demo.auth;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.demo.entity.LoginUser;
+
+public class AuthUserDetails implements UserDetails {
+
+	// 認証するユーザー情報
+	private final LoginUser loginUser;
+	private final Collection<? extends GrantedAuthority> authorities;
+	
+	// コンストラクタ 認証したい任意のユーザー情報を指定
+	public AuthUserDetails(LoginUser loginUser) {
+		this.loginUser = loginUser;
+		// 仮の処理
+		List<String> list = new ArrayList<String>(Arrays.asList(loginUser.getUsername()));
+		this.authorities = list
+				.stream()
+				.map(role -> new SimpleGrantedAuthority(role))
+				.toList();
+	}
+	
+	// テンプレート内で ${#authentication.principal.loginUser} アクセスできるようにするため
+	public LoginUser getLoginUser() {
+		return loginUser;
+	}
+	
+	
+	// 各メソッドをオーバーライド
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO 自動生成されたメソッド・スタブ
+		//return null;
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO 自動生成されたメソッド・スタブ
+		//return null;
+		return loginUser.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO 自動生成されたメソッド・スタブ
+		//return null;
+		return loginUser.getUsername();
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO 自動生成されたメソッド・スタブ
+		//return false;
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO 自動生成されたメソッド・スタブ
+		//return false;
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO 自動生成されたメソッド・スタブ
+		//return false;
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO 自動生成されたメソッド・スタブ
+		//return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/example/demo/service/AuthUserDetailsServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.auth.AuthUserDetails;
+import com.example.demo.entity.LoginUser;
+import com.example.demo.entity.LoginUserMapper;
+
+import lombok.RequiredArgsConstructor;
+
+// UserDetailsService 拡張認証用サービスクラス
+
+@RequiredArgsConstructor
+@Service
+public class AuthUserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired 
+    private LoginUserMapper loginUserMapper;
+
+    // loadUserByUsername のオーバーライド UserDetails オブジェクトを返却する
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    	
+    	System.out.println("===========");
+    	System.out.println(username);
+        //findByUsernameで見つけてきたユーザ情報
+        LoginUser loginUser = loginUserMapper.findByUsername(username);
+        System.out.println("===========");
+        System.out.println(loginUser);
+        
+    	AuthUserDetails user = new AuthUserDetails(loginUser);
+    	System.out.println(user);
+		return user;
+
+    }
+}
